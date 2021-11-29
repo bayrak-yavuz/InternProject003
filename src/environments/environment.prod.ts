a89c1d781309a327e5f3adb3a15746d028205217
Mustafa Alperen Karakuş <alperenkarakus062002@gmail.com>
2021-11-29 14:21:50 +0300

    patladıkee

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true
+};
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC41HzZlNIfmp8JR2ed85pCQbZseKpMav8",
+  authDomain: "ionicneyesem.firebaseapp.com",
+  projectId: "ionicneyesem",
+  storageBucket: "ionicneyesem.appspot.com",
+  messagingSenderId: "1026798085847",
+  appId: "1:1026798085847:web:ab032507ace5c4cbbd7292",
+  measurementId: "G-4ZK6ZFD19D"
+};
