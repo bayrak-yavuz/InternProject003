6026dff7804a0b337e44ae095321e78a5c02c7aa
durdane ayşe <78816462+DurdaneAyse@users.noreply.github.com>
2021-12-02 16:34:36 +0300

    logout

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -12,81 +12,73 @@ import { Location } from '@angular/common';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  name:string;
-  email:string;
-  phone:string;
-  password:any;
+  name: string;
+  email: string;
+  phone: string;
+  password: any;
 
   constructor(
-    private afs:AngularFirestore,
-    private afaut:AngularFireAuth,
-    private router:Router,
+    private afs: AngularFirestore,
+    private afaut: AngularFireAuth,
+    private router: Router,
     private loadingCtrl: LoadingController,
     private toastr: ToastController,
-    private loginService:LoginService,
+    private loginService: LoginService,
     private location: Location,
 
-    
+
   ) { }
   goBack(): void {
     this.location.back();
 
   }
-  ngOnInit() {} 
- async register()
- {
-   if(this.name && this.email && this.phone && this.password){
-     const loading=await this.loadingCtrl.create({
-      message:'Yükleniyor...',
-      spinner:'crescent',
-      showBackdrop:true
-     });
+  ngOnInit() { }
+  async register() {
+    if (this.name && this.email && this.phone && this.password) {
+      const loading = await this.loadingCtrl.create({
+        message: 'Yükleniyor...',
+        spinner: 'crescent',
+        showBackdrop: true
+      });
 
-     loading.present();
+      loading.present();
 
-     this.afaut.createUserWithEmailAndPassword(this.email, this.password)
-     .then((data)=>{
-       this.afs.collection('user').doc(data.user.uid).set({
-       'userId':data.user.uid,
-       'userName':this.name,
-       'userEmail':this.email,
-       'userPhone':this.phone,
-       'createdAt': Date.now()
-       })
-       .then(()=> {
-         loading.dismiss();
-<<<<<<< HEAD
-         this.toast('Kayıt başarılı lütfen e-postanızı kontrol edin!','success');
-         this.router.navigate(['tabs/tab3']);
-=======
-         this.toast('Kayıt başarılı lütfen e-postanızı kontrl edin!','success');
-         this.router.navigate(['/login']);
->>>>>>> c1c16f6da6a9658d1a3798b3cd3a1f2cd5418749
-       })
-       .catch(error =>{
-        loading.dismiss();
-        this.toast(error.message,'darger');
-       })
-     })
-   }
-<<<<<<< HEAD
-   else{
-    this.toast('Bilgileri tam','success');
+      this.afaut.createUserWithEmailAndPassword(this.email, this.password)
+        .then((data) => {
+          this.afs.collection('user').doc(data.user.uid).set({
+            'userId': data.user.uid,
+            'userName': this.name,
+            'userEmail': this.email,
+            'userPhone': this.phone,
+            'createdAt': Date.now()
+          })
+            .then(() => {
+              loading.dismiss();
 
-   }
-=======
->>>>>>> c1c16f6da6a9658d1a3798b3cd3a1f2cd5418749
- }//end register
+              this.toast('Kayıt başarılı lütfen e-postanızı kontrol edin!', 'success');
+              this.router.navigate(['tabs/login']);
+            })
+            .catch(error => {
+              loading.dismiss();
+              this.toast(error.message, 'darger');
+            })
+        })
+        
+    }
+    else {
+      this.toast('Bilgileri tam  giriniz', 'success');
 
-async toast(message,status)
-{
-  const toast=await this.toastr.create({
-    message:message,
-    color:status,
-    position:'top',
-    duration:2000,
-  }) ;
-  toast.present();
-}
+    }
+  }//end register
+
+  async toast(message, status) {
+    const toast = await this.toastr.create({
+      message: message,
+      color: status,
+      position: 'top',
+      duration: 2000,
+    });
+    toast.present();
+  }
 
 }
