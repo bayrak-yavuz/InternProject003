ed6070f1806390678bd838ca081f91a8654a649d
Mustafa Alperen Karakuş <alperenkarakus062002@gmail.com>
2021-12-07 13:00:48 +0300

    tarifler sayfası oluşturuldu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'recipes',
+    loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
   }
+
 
   
   
