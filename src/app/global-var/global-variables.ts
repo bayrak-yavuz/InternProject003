ec562e59ee2cdfd8967b93b11323c1a0d017bec5
Berke Erçetin <82932880+berkeercetin@users.noreply.github.com>
2021-12-08 16:23:19 +0300

    test

--- src/app/global-var/global-variables.ts
+++ src/app/global-var/global-variables.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 @Injectable()
 export class GlobalVariables {
-    public static log:boolean 
+    public static log:boolean=true
     recipesIndex:number =-1;
    
 }
