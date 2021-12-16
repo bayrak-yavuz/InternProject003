ad6e7d72e574d7c4d8fb6f0f331affdafdd3674d
durdane ayşe <78816462+DurdaneAyse@users.noreply.github.com>
2021-12-16 11:15:37 +0300

    photo

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 import { LoginService } from '../services/login.service';
-
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+export interface imageData{
+  fileName:string;
+  filePath:string;
+  size:string;
+}
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -11,12 +18,21 @@ import { LoginService } from '../services/login.service';
 })
 export class Tab3Page {
 user:any;
+fileName:string;
+fileSize:string;
+isLoading:boolean;
+isLoaded:boolean;
+private imageCollection:AngularFirestoreCollection<imageData>;
   constructor(
     private login:LoginService,
     private router:Router,
     private toastr:ToastController,
-
-  ) {}
+    private storage:AngularFireStorage,
+    private database:AngularFirestore,
+  ) {
+    this.isLoaded=false;
+    this.isLoading=false;
+  }
 
 logout()
 {
@@ -41,6 +57,17 @@ ngOnInit()
   this.login.user$.subscribe(user=>{
     this.user=user;
    })
+}
+addImage(event){
+  const file = event.target.files;
+  console.log(file);
+   var fileName=file[0];
+  console.log(fileName);
+
+  if(fileName.type.split('/')[0] !=="image"){
+    console.error("file is not image");
+    return;
+  }
 }
 }
 
